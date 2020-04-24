3277b8fb2ff30138003dfdf20c61478ac0eb3178
Alexander Jiang <dashuai99@gmail.com>
2020-04-23 22:18:21 -0700

    check for duplicate tweets when extracting features

--- extract_features.py
+++ extract_features.py
@@ -16,19 +16,26 @@ csv_writer = csv.DictWriter(csvfile, fieldnames=TweetFeatures.get_field_names(),
 csv_writer.writeheader()
 
 total_num_tweets = 0
+tweet_ids = set([])
 filenames = glob.glob("realDonaldTrump_tweets/*.json")
 for filename in filenames:
+    print(f"reading tweets from {filename}")
     with open(filename, "r", encoding="utf8") as f:
         tweets_json = json.load(f)
 
     total_num_tweets += len(tweets_json)
-
     for tweet in tweets_json:
         tweet_datetime = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S %z %Y")
         eastern_time = tweet_datetime.astimezone(eastern_tz)
-        print(f"tweet id {tweet['id']} at {eastern_time}")
-
+        # print(f"tweet id {tweet['id']} at {eastern_time}")
+        
         tweet_features = TweetFeatures(tweet)
+        if tweet_features.id in tweet_ids:
+            print(f"found duplicate tweet ID! {tweet_features.id}")
+            break
+        tweet_ids.add(tweet_features.id)
+
         csv_writer.writerow(tweet_features.as_dict())
 
 csvfile.close()
+print(f"extracted features for {total_num_tweets} tweets")
