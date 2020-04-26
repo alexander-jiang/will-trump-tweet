aff291d59dbc9433409781078299a712ea7d2fe7
Kevin Wu <kevin.wu@columbia.edu>
2020-04-25 19:24:05 -0500

    additoinal parameters for analysis

--- twitter_fetcher/tweet_features.py
+++ twitter_fetcher/tweet_features.py
@@ -13,11 +13,14 @@ class TweetFeatures:
         self.source = tweet_json['source']
         self.is_retweet = 'retweeted_status' in tweet_json
         self.text_len = len(tweet_json['text'])
+        self.text = tweet_json['text']
+        self.favorite = tweet_json['favorite_count']
+        self.retweets = tweet_json['retweet_count']
 
     # List of field names (used for CSV header)
     @classmethod
     def get_field_names(cls):
-        return ['id', 'created_at', 'source', 'is_retweet', 'text_len']
+        return ['id', 'created_at', 'source', 'is_retweet', 'text_len', 'text', 'favorite', 'retweets']
 
     # This function can be used as input for csv.DictWriter.writerow()
     def as_dict(self):
@@ -26,5 +29,8 @@ class TweetFeatures:
             'created_at': self.created_at,
             'source': self.source,
             'is_retweet': self.is_retweet,
-            'text_len': self.text_len
+            'text_len': self.text_len,
+            'text' : self.text,
+            'favorite' : self.favorite,
+            'retweets' : self.retweets
         }
